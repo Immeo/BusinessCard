8533a6f5b0a3bff27472cac8a49320f0c41b5489
Meo <wayfarer245@gmail.com>
2020-11-30 04:57:56 +0300

    prerease 1.4

--- CardApp/models.py
+++ CardApp/models.py
@@ -107,6 +107,95 @@ class edit_text(models.Model):
         blank=True,
         null=True,
     )
+    AboutSectionH2 = models.CharField(
+        verbose_name='Изменить название тега h2 во второй секции',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    AboutSectionP = models.TextField(
+        verbose_name='Изменить текст под тегом h2 во второй секции',
+        blank=True,
+        null=True,
+    )
+    AboutSectionTextArea1 = models.TextField(
+        verbose_name='Изменить текст первой колонки во второй секции',
+        blank=True,
+        null=True,
+    )
+    AboutSectionTextArea2 = models.TextField(
+        verbose_name='Изменить текст второй колонки во второй секции',
+        blank=True,
+        null=True,
+    )
+    AboutLi1 = models.CharField(
+        verbose_name='Изменить текст пункта второго сектора 1',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    AboutLi2 = models.CharField(
+        verbose_name='Изменить текст пункта второго сектора 2',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    AboutLi3 = models.CharField(
+        verbose_name='Изменить текст пункта второго сектора 3',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    AboutButton = models.CharField(
+        verbose_name='Изменить текст кнопки второго сектора',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    AboutCounterNumber1 = models.IntegerField(
+        verbose_name='Изменить цифры счетчика второго сектора 1',
+        blank=True,
+        null=True,
+    )
+    AboutCounterTitle1 = models.CharField(
+        verbose_name='Изменить текст под счетчиком второго сектора 1',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    AboutCounterNumber2 = models.IntegerField(
+        verbose_name='Изменить цифры счетчика второго сектора 2',
+        blank=True,
+        null=True,
+    )
+    AboutCounterTitle2 = models.CharField(
+        verbose_name='Изменить текст под счетчиком второго сектора 2',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    AboutCounterNumber3 = models.IntegerField(
+        verbose_name='Изменить цифры счетчика второго сектора 3',
+        blank=True,
+        null=True,
+    )
+    AboutCounterTitle3 = models.CharField(
+        verbose_name='Изменить текст под счетчиком второго сектора 3',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    AboutCounterNumber4 = models.IntegerField(
+        verbose_name='Изменить цифры счетчика второго сектора 4',
+        blank=True,
+        null=True,
+    )
+    AboutCounterTitle4 = models.CharField(
+        verbose_name='Изменить текст под счетчиком второго сектора 4',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.TopFirstH1
